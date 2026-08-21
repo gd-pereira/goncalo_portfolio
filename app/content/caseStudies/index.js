6dd7103257d0356bb5c23f4262fb7fb004bc0094
Gonçalo Pereira <goncalo.dinis.spe@gmail.com>
2026-08-21 18:55:49 +0200

    small fixes

--- app/content/caseStudies/index.js
+++ app/content/caseStudies/index.js
@@ -1,29 +1,15 @@
 import { terminumCaseStudy } from "./terminum";
-import { theGlobalScaleCaseStudy } from "./the-global-scale";
-import { leBarometreDuCoeurCaseStudy } from "./le-barometre-du-coeur";
 
-const caseStudies = [
-  terminumCaseStudy,
-  theGlobalScaleCaseStudy,
-  leBarometreDuCoeurCaseStudy,
-];
+const caseStudies = [terminumCaseStudy];
 
 export function getAllCaseStudies() {
   return caseStudies;
 }
 
-const PUBLISHED_SLUGS = new Set(["terminum"]);
-
 export function getCaseStudyBySlug(slug) {
-  if (!PUBLISHED_SLUGS.has(slug)) {
-    return null;
-  }
-
   return caseStudies.find((study) => study.slug === slug) ?? null;
 }
 
 export function getCaseStudySlugs() {
-  return caseStudies
-    .map((study) => study.slug)
-    .filter((slug) => PUBLISHED_SLUGS.has(slug));
+  return caseStudies.map((study) => study.slug);
 }
